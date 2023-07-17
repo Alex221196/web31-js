d1a4e4132234308603a6bf0a6fe59fd0e96dfb5a
John Doe <fillipp22@gmail.com>
2023-07-17 21:15:17 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,38 @@
 import './style.css'
 
+// Запросите у пользователя число, возведите это число во
+// 2-ю степень и выведите на экран.
+// let input = prompt('Введите число', '0') 
+// alert(input**2)
+
+// Запросите у пользователя 2 числа и выведите среднее ариф-
+// метическое этих чисел.
+// let input = parseFloat( prompt('Введите число', '0') as string)
+// let inputT = parseFloat( prompt('Введите число', '0') as string)
+// alert((input + inputT)/2)
+ 
+// зфдфние 3
+
+// let user = prompt('Введите число', '0' )
+// alert ( user * user)
+
+// задание 4
+
+// let input = prompt('Введите количесво километров', '0')
+// const mili = 0.621371
+// alert (input * mili)
+
+// задача 5
+
+// let input = parseFloat( prompt( 'Введите число', '0') as string)
+// let inputT = parseFloat( prompt('Введите число', '0') as string)
+
+// alert(`${input + inputT} ${input - inputT} ${input * inputT} ${input / inputT}`)
+
+// Зфдача 6
+
+
+
+
+
+за
